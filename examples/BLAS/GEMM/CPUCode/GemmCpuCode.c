20b4175d9f8f6c6a63152b961b22c03672d873cb
Chris Jones <chris.jones@cantab.net>
2015-02-06 11:14:28 +0000

    Matrix multiply improvements:  - use stream offset, rather than counter to preload tile in TileMultiplier  - simplify TileMultiplier to only handle square tiles  - overlap CPU accumulation with DFE compute

--- examples/BLAS/GEMM/CPUCode/GemmCpuCode.c
+++ examples/BLAS/GEMM/CPUCode/GemmCpuCode.c
@@ -7,6 +7,7 @@
 #include "DGEMM.h"
 
 #define TILE_SIZE DGEMM_tileSize
+#define TILE_SIZE_2D (TILE_SIZE * TILE_SIZE)
 #define FREQUENCY DGEMM_frequency
 
 #define USE_BLAS
@@ -35,24 +36,48 @@ void dgemm_model(
 #endif
 }
 
+static size_t min(size_t a, size_t b) {
+	return (a < b) ? a : b;
+}
+
+static size_t sizeInTiles(size_t n) {
+	return (n + TILE_SIZE - 1) / TILE_SIZE;
+}
+
 max_file_t* maxfile;
 max_engine_t* engine;
-max_actions_t* actions;
+max_actions_t* actionsIn;
+max_actions_t* actionsOut0;
+max_actions_t* actionsOut1;
 const max_handle_t* aHandle;
 const max_handle_t* bHandle;
 const max_handle_t* cHandle;
-struct timespec run_start;
-struct timespec run_finish;
+double* aIn;
+double* bIn;
+double* cOut0;
+double* cOut1;
 
-void dgemm_init() {
+void dgemm_init(size_t mMax, size_t nMax, size_t kMax) {
 	printf("Initializing maxfile...\n");
-	maxfile  = DGEMM_init();
-	aHandle  = max_get_handle_stream(maxfile, "A");
-	bHandle  = max_get_handle_stream(maxfile, "B");
-	cHandle  = max_get_handle_stream(maxfile, "C");
+	maxfile = DGEMM_init();
+	aHandle = max_get_handle_stream(maxfile, "A");
+	bHandle = max_get_handle_stream(maxfile, "B");
+	cHandle = max_get_handle_stream(maxfile, "C");
+
 	printf("Loading engine...\n");
-	engine   = max_load(maxfile, "*");
-	actions  = max_actions_init(maxfile, NULL);
+	engine = max_load(maxfile, "*");
+
+	actionsIn   = max_actions_init(maxfile, NULL);
+	actionsOut0 = max_actions_init_explicit(maxfile);
+	actionsOut1 = max_actions_init_explicit(maxfile);
+
+	size_t mTiles = sizeInTiles(mMax);
+	size_t nTiles = sizeInTiles(nMax);
+	size_t kTiles = sizeInTiles(kMax);
+	aIn   = malloc(mTiles * kTiles * TILE_SIZE_2D * sizeof(double));
+	bIn   = malloc(kTiles * nTiles * TILE_SIZE_2D * sizeof(double));
+	cOut0 = malloc(kTiles * TILE_SIZE_2D * sizeof(double));
+	cOut1 = malloc(kTiles * TILE_SIZE_2D * sizeof(double));
 }
 
 void dgemm(
@@ -61,16 +86,9 @@ void dgemm(
 {
 	// TODO support transform
 
-	size_t mTiles = (m + TILE_SIZE - 1) / TILE_SIZE;
-	size_t nTiles = (n + TILE_SIZE - 1) / TILE_SIZE;
-	size_t kTiles = (k + TILE_SIZE - 1) / TILE_SIZE;
-
-	size_t numTiles   = mTiles * nTiles * kTiles;
-	size_t tileSize2D = TILE_SIZE * TILE_SIZE;
-
-	double* aIn  = malloc(mTiles * kTiles * tileSize2D * sizeof(double));
-	double* bIn  = malloc(kTiles * nTiles * tileSize2D * sizeof(double));
-	double* cOut = malloc(mTiles * nTiles * kTiles * tileSize2D * sizeof(double));
+	size_t mTiles = sizeInTiles(m);
+	size_t nTiles = sizeInTiles(n);
+	size_t kTiles = sizeInTiles(k);
 
 	// re-order A and B matrices into tiles and pad them up to size
 	size_t pos = 0;
@@ -113,47 +131,70 @@ void dgemm(
 		}
 	}
 
-	max_clear_queues(actions);
-	max_set_ticks(actions, "TM", (numTiles+1) * tileSize2D);
-	max_set_uint64t(actions, "TM", "numTiles", numTiles);
+	max_clear_queues(actionsIn);
+	max_set_ticks(actionsIn, "TM", mTiles * nTiles * kTiles * TILE_SIZE_2D);
 
 	for (size_t mm = 0; mm < mTiles; ++mm) {
 		for (size_t nn = 0; nn < nTiles; ++nn) {
-			max_queue_input_handle(actions, aHandle, &aIn[mm*kTiles*tileSize2D], kTiles * tileSize2D * sizeof(double));
+			max_queue_input_handle(actionsIn, aHandle, &aIn[mm*kTiles*TILE_SIZE_2D], kTiles * TILE_SIZE_2D * sizeof(double));
 		}
 	}
 
 	for (size_t mm = 0; mm < mTiles; ++mm) {
-		max_queue_input_handle(actions, bHandle, bIn, kTiles * nTiles * tileSize2D * sizeof(double));
+		max_queue_input_handle(actionsIn, bHandle, bIn, kTiles * nTiles * TILE_SIZE_2D * sizeof(double));
 	}
 
-	max_queue_output_handle(actions, cHandle, cOut, numTiles * tileSize2D * sizeof(double));
+	max_disable_stream_sync(actionsIn, "A");
+	max_disable_stream_sync(actionsIn, "B");
 
-	clock_gettime(CLOCK_REALTIME, &run_start);
-	max_run(engine, actions);
-	clock_gettime(CLOCK_REALTIME, &run_finish);
+	max_run(engine, actionsIn);
 
-	pos = 0;
+	max_clear_queues(actionsOut0);
+	max_queue_output_handle(actionsOut0, cHandle, cOut0, kTiles * TILE_SIZE_2D * sizeof(double));
+	max_sync_stream(actionsOut0, "C");
+
+	max_clear_queues(actionsOut1);
+	max_queue_output_handle(actionsOut1, cHandle, cOut1, kTiles * TILE_SIZE_2D * sizeof(double));
+	max_sync_stream(actionsOut1, "C");
+
+	max_run_t* run0 = max_run_nonblock(engine, actionsOut0);
+	max_run_t* run1 = max_run_nonblock(engine, actionsOut1);
+
+	size_t tile = 2;
 	for (size_t mm = 0; mm < mTiles; ++mm) {
-		for (size_t nn = 0; nn < nTiles; ++nn) {
+		size_t xMax = min(TILE_SIZE, m - mm*TILE_SIZE);
+
+		for (size_t nn = 0; nn < nTiles; ++nn, ++tile) {
+			size_t yMax = min(TILE_SIZE, n - nn*TILE_SIZE);
+
+			for (size_t x = 0; x < xMax; ++x) {
+				size_t row = mm*TILE_SIZE + x;
+
+				for (size_t y = 0; y < yMax; ++y) {
+					size_t col = nn*TILE_SIZE + y;
+					C[row*ldc+col] *= beta;
+				}
+			}
+
+			max_wait((tile & 1) ? run1 : run0);
+			const double* cOut = (tile & 1) ? cOut1 : cOut0;
+
 			for (size_t kk = 0; kk < kTiles; ++kk) {
-				for (size_t x = 0; x < TILE_SIZE; ++x) {
+				for (size_t x = 0; x < xMax; ++x) {
 					size_t row = mm*TILE_SIZE + x;
-					if (row >= m) {
-						pos += (TILE_SIZE - x) * TILE_SIZE;
-						break;
-					}
 
-					for (size_t y = 0; y < TILE_SIZE; ++y) {
+					for (size_t y = 0; y < yMax; ++y) {
 						size_t col = nn*TILE_SIZE + y;
-						if (col >= n) {
-							pos += TILE_SIZE - y;
-							break;
-						}
-
-						if (kk == 0) C[row*ldc+col] *= beta;
-						C[row*ldc+col] += alpha * cOut[pos++];
+						C[row*ldc+col] += alpha * cOut[TILE_SIZE_2D*kk + TILE_SIZE*x + y];
 					}
+				}
+			}
+
+			if (tile < mTiles*nTiles) {
+				if (tile & 1) {
+					run1 = max_run_nonblock(engine, actionsOut1);
+				} else {
+					run0 = max_run_nonblock(engine, actionsOut0);
 				}
 			}
 		}
@@ -186,8 +227,6 @@ static int check_result(int m, int n, const double* expected, const double* actu
 }
 
 int main(int argc, char** argv) {
-	dgemm_init();
-
 	unsigned seed = time(NULL);
 	printf("Random seed: %u\n", seed);
 	srand(seed);
@@ -197,9 +236,9 @@ int main(int argc, char** argv) {
 	int k;
 
 	if (argc == 1) {
-		m = random() % (5 * TILE_SIZE);
-		n = random() % (5 * TILE_SIZE);
-		k = random() % (5 * TILE_SIZE);
+		m = (random() % (5 * TILE_SIZE)) + TILE_SIZE;
+		n = (random() % (5 * TILE_SIZE)) + TILE_SIZE;
+		k = (random() % (5 * TILE_SIZE)) + TILE_SIZE;
 	} else if (argc == 2) {
 		m = n = k = atoi(argv[1]);
 	} else {
@@ -214,19 +253,22 @@ int main(int argc, char** argv) {
 
 	printf("Matrix dimensions: m = %d, n = %d, k = %d\n", m, n, k);
 
-	size_t mTiles = (m + TILE_SIZE - 1) / TILE_SIZE;
-	size_t nTiles = (n + TILE_SIZE - 1) / TILE_SIZE;
-	size_t kTiles = (k + TILE_SIZE - 1) / TILE_SIZE;
+	dgemm_init(m, n, k);
+
+	size_t mTiles = sizeInTiles(m);
+	size_t nTiles = sizeInTiles(n);
+	size_t kTiles = sizeInTiles(k);
 
 	printf("DFE tile size: %d\n", TILE_SIZE);
 	printf("DFE compute dimensions: m = %zu, n = %zu, k = %zu\n",
 			mTiles * TILE_SIZE, nTiles * TILE_SIZE, kTiles * TILE_SIZE);
 
 	double cpuPoints = ((double) m) * n * k;
-	double dfePoints = ((double) mTiles) * nTiles * kTiles * TILE_SIZE * TILE_SIZE * TILE_SIZE;
-	printf("DFE compute efficiency: %f\n", cpuPoints / dfePoints);
+	double dfePoints = ((double) mTiles) * nTiles * kTiles * TILE_SIZE_2D;
+	printf("DFE compute efficiency: %f\n", cpuPoints / (dfePoints * TILE_SIZE));
 	printf("DFE frequency: %d MHz\n", FREQUENCY);
-	printf("DFE predicted compute time: %f s\n", dfePoints / (((double) TILE_SIZE) * FREQUENCY * 1000000));
+	// add one tile to account for stream offset TODO measure pipeline depth properly?
+	printf("DFE predicted compute time: %f s\n", (dfePoints + TILE_SIZE_2D) / (((double) FREQUENCY) * 1e6));
 
 	for (int i = 0; i < m*k; ++i) {
 		A[i] = random() % 100;
@@ -248,17 +290,17 @@ int main(int argc, char** argv) {
 	struct timespec start;
 	struct timespec finish;
 
-	printf("Running HW... "); fflush(stdout);
-	clock_gettime(CLOCK_REALTIME, &start);
-	dgemm("n", "n", m, n, k, alpha, A, k, B, n, beta, Chw, n);
-	clock_gettime(CLOCK_REALTIME, &finish);
+	for (int xx = 0; xx < 5; ++xx) {
+		printf("Repeat %d\n", xx);
+		printf("Running HW... "); fflush(stdout);
+		clock_gettime(CLOCK_REALTIME, &start);
+		dgemm("n", "n", m, n, k, alpha, A, k, B, n, beta, Chw, n);
+		clock_gettime(CLOCK_REALTIME, &finish);
 
-	struct timespec hw_time  = diff(start, finish);
-	struct timespec dfe_time = diff(run_start, run_finish);
-	struct timespec cpu_time = diff(dfe_time, hw_time);
+		struct timespec hw_time  = diff(start, finish);
 
-	printf("took: %ld.%09ld s (DFE time: %ld.%09ld s, CPU time: %ld.%09ld s)\n",
-			hw_time.tv_sec, hw_time.tv_nsec, dfe_time.tv_sec, dfe_time.tv_nsec, cpu_time.tv_sec, cpu_time.tv_nsec);
+		printf("took: %ld.%09ld s\n", hw_time.tv_sec, hw_time.tv_nsec);
+	}
 
 	printf("Running SW... "); fflush(stdout);
 	clock_gettime(CLOCK_REALTIME, &start);
